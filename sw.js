32a5a1d6f651b9534412cc98d2cb2b3aa8489bbb
furugenminamifc <furugen.minamifc@gmail.com>
2026-08-06 20:44:52 +0900

    古堅南FC ゲームコレクション Ver.4.1 PKチャレンジ プレミアム完全版

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='furugen-game-v4-premium';
+const CACHE='furugen-game-v41-complete-4101';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
